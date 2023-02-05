e9ad6fee074ea545ea8dfd748e8e6d5b94e72e5c
Akshaay9 <nair.akshay9898@gmail.com>
2023-02-05 12:17:21 +0530

    area selector

---
+++ gridPixelArt/script.js
@@ -0,0 +1,79 @@
+// 7,11
+
+class GridPixelArt {
+  constructor(row, column) {
+    //  Constant and Variable
+    this.row = row;
+    this.column = column;
+
+    this.startPointRow = null;
+    this.startPointColumn = null;
+
+    this.endPointRow = null;
+    this.endPointColumn = null;
+
+    //  Selectos
+    this.board = document.getElementById("board");
+
+    // funcitons
+    this.generateBoard();
+    this.bindEVvent();
+  }
+  bindEVvent() {
+    this.board.addEventListener("mousedown", (e) => {
+      const className = e.target.className;
+      if (className === "column") {
+        const [row, column] = e.target.dataset.id.split("_");
+        this.startPointRow = row;
+        this.startPointColumn = column;
+        this.colorBoard();
+      }
+    });
+  }
+  mouseEnterColumn(e) {
+    const className = e.target.className;
+    if (this.startPointRow && this.startPointColumn && className === "column") {
+      const [row, column] = e.target.dataset.id.split("_");
+      this.endPointRow = row;
+      this.endPointColumn = column;
+      console.log({
+        startPountRow: this.startPointRow,
+        startPountCol: this.startPointRow,
+        endPointRow: this.endPointRow,
+        endPointColumn: this.endPointColumn,
+      });
+      this.colorBoard();
+    }
+  }
+
+  colorBoard() {
+    for (let i = this.startPointRow; i <= this.endPointRow; i++) {
+      for (let j = this.startPointColumn; j <= this.endPointColumn; j++) {
+        const key = `${i}_${j}`;
+
+        const column = document.querySelector(`[data-id="${key}"]`);
+        column.classList.add("skyBlue");
+      }
+    }
+  }
+
+  generateBoard() {
+    for (let i = 0; i < this.row; i++) {
+      const row = document.createElement("div");
+      row.classList.add("row");
+      row.dataset["rowId"] = i;
+      for (let j = 0; j < this.column; j++) {
+        const column = document.createElement("div");
+        column.classList.add("column");
+        column.dataset["id"] = `${i}_${j}`;
+        column.addEventListener("mouseenter", (e) => {
+          this.mouseEnterColumn(e);
+        });
+        row.appendChild(column);
+      }
+      this.board.appendChild(row);
+    }
+  }
+}
+
+new GridPixelArt(7, 11);
