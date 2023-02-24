68ba7c505727104e904256ddace2dafe09162062
Akshaay9 <nair.akshay9898@gmail.com>
2023-02-24 19:08:02 +0530

    completed calendar

--- calendarDevKode/script.js
+++ calendarDevKode/script.js
@@ -1,10 +1,21 @@
 class CalendarDevKode {
   constructor(data) {
-    this.data = data;
+    this.data = this.sortData(data);
     this.calendarContainer = document.getElementById("calendarContainer");
+    this.conflictData = [];
     this.generateUi();
-
     this.placeCard(this.data);
+  }
+
+  sortData(data) {
+    const newData = data.sort((a, b) => {
+      const [hour, minute] = a.startTime.split(":");
+      const [hourb, minuteb] = b.startTime.split(":");
+      const totalStartTimeA = Number(hour) + Number(minute) / 60;
+      const totalStartTimeB = Number(hourb) + Number(minuteb) / 60;
+      return totalStartTimeA - totalStartTimeB;
+    });
+    return newData;
   }
 
   generateUi() {
@@ -21,6 +32,26 @@ class CalendarDevKode {
     }
   }
 
+  getConflictTime(data) {
+    let conflictingLen = 0;
+
+    const { startTime } = data;
+    const [startHour, startMinute] = startTime.split(":");
+    const totoalStartTime = Number(startHour) + Number(startMinute) / 60;
+
+    for (let conflictData of this.conflictData) {
+      const { endTime } = conflictData;
+      const [endHour, endMinute] = endTime.split(":");
+      const totoalEndTime = Number(endHour) + Number(endMinute) / 60;
+      if (totoalStartTime < totoalEndTime) {
+        conflictingLen += 1;
+      }
+    }
+    this.conflictData.push(data);
+
+    return conflictingLen;
+  }
+
   getTopPositionInPixel = (startTime) => {
     const [hour, minute] = startTime.split(":");
     const topPosition = Number(hour) + Number(minute / 60);
@@ -31,22 +62,28 @@ class CalendarDevKode {
     const [startHour, startMinute] = startTime.split(":");
     const [endHour, endMinute] = endTime.split(":");
     const startTingHour = Number(endHour) - Number(startHour);
-    const startingMinute = Number(endMinute) - Number(startMinute);
-    const height = (startTingHour + startingMinute / 60) * 60;
-    console.log(startTime, endTime, height);
+    const startingMinute = (Number(endMinute) - Number(startMinute)) / 60;
+    const height = startTingHour + startingMinute;
     return height;
   };
 
   placeCard(data) {
     data.forEach((ele) => {
-      const { startTime, endTime, title } = ele;
+      const { startTime, endTime, title, color } = ele;
       const topPosition = this.getTopPositionInPixel(startTime);
       const totalHeight = this.getHeight(startTime, endTime);
       const timeCard = document.createElement("div");
       timeCard.innerHTML = `${startTime}-${endTime}-->titlle ${title}  `;
       timeCard.classList.add("timeCard");
       timeCard.style.top = `${topPosition * 52}px`;
-      timeCard.style.height = `${totalHeight}px`;
+      timeCard.style.background = color;
+      timeCard.style.height = `${totalHeight * 60}px`;
+      // Conlficting time
+      const conflictLen = this.getConflictTime(ele);
+      const left = `${40 + 50 * conflictLen}px`;
+      timeCard.style.marginLeft = `${left}`;
+
+      //
       this.calendarContainer.appendChild(timeCard);
     });
   }
@@ -54,15 +91,33 @@ class CalendarDevKode {
 
 const data = [
   {
-    startTime: "0:00",
-    endTime: "1:30",
+    startTime: "00:00",
+    endTime: "01:30",
     color: "#f6be23",
     title: "#TeamDevkode",
   },
   {
-    startTime: "4:30",
+    startTime: "3:30",
     endTime: "7:30",
     color: "#f6501e",
+    title: "#TeamDevkode",
+  },
+  {
+    startTime: "4:30",
+    endTime: "8:30",
+    color: "#f6501e",
+    title: "#TeamDevkode",
+  },
+  {
+    startTime: "6:30",
+    endTime: "9:00",
+    color: "#f6501e",
+    title: "Demo",
+  },
+  {
+    startTime: "11:00",
+    endTime: "13:30",
+    color: "#029be5",
     title: "#TeamDevkode",
   },
   {
@@ -72,14 +127,32 @@ const data = [
     title: "#TeamDevkode",
   },
   {
-    startTime: "9:00",
-    endTime: "10:00",
+    startTime: "9:30",
+    endTime: "10:30",
     color: "#029be5",
     title: "#TeamDevkode",
   },
   {
     startTime: "16:00",
+    endTime: "17:00",
+    color: "#029be5",
+    title: "#TeamDevkode",
+  },
+  {
+    startTime: "15:00",
+    endTime: "17:00",
+    color: "#029be5",
+    title: "#TeamDevkode",
+  },
+  {
+    startTime: "18:00",
     endTime: "19:00",
+    color: "#f6501e",
+    title: "#TeamDevkode",
+  },
+  {
+    startTime: "20:30",
+    endTime: "22:30",
     color: "#029be5",
     title: "#TeamDevkode",
   },
